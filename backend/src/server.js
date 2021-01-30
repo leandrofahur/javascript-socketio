513605bfb6ed409ebc5825682714e1339391ba82
leandro machado <fahurleandro@gmail.com>
2021-01-30 14:51:54 -0800

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
+const IP = process.env.IP || "0.0.0.0";
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -14,6 +17,9 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(port, "0.0.0.0", () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+http.listen(PORT, IP, () => {
+  console.log(`Socket.IO server running at http://:${IP}:${PORT}`);
 });
+
+// port forwarding
+// socketio
